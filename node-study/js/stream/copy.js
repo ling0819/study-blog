1c1635aae36956309c7f4d0d0b058c40a6f07f94
ling0819 <zxyling@126.com>
2019-05-10 23:43:16 +0800

    .

--- node-study/js/stream/copy.js
+++ node-study/js/stream/copy.js
@@ -1,26 +1,28 @@
 let fs = require('fs');
-let {Readable} = require('stream');
+let {Readable, Writable} = require('stream');
 
 
-class MyReadStream extends Readable {
-    constructor() {
-        super();
-        this.index = 8;
-    }
+// 父类会调用子类的_read方法
+// http基于流的req.on('data')
+// class MyReadStream extends Readable {
+//     constructor() {
+//         super();
+//         this.index = 8;
+//     }
+//     // fs.createReadStream 重写了_read方法
+//     _read() {
+//         if (this.index-- == 0) {
+//             return this.push(null);
+//         }
 
-    _read() {
-        if (this.index-- == 0) {
-            return this.push(null);
-        }
+//         this.push(this.index + '');
+//     }
+// }
 
-        this.push(this.index + '');
-    }
-}
-
-let rs = new MyReadStream();
-rs.on('data', function(chunk){
-    console.log(chunk);
-})
+// let rs = new MyReadStream();
+// rs.on('data', function(chunk){
+//     console.log(chunk);
+// })
 
 
 
@@ -37,3 +39,13 @@ rs.on('data', function(chunk){
 // ws.write('123');
 // ws.end('123'); 会强制把内存中的数据清空 并且关闭文件   文件已关闭就不能继续写入 如果end中有参数就会继续调用write方法  fs.close
 // ws.end();
+
+
+class MyWriteStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(chunk);
+    }
+}
+
+let ws
+
