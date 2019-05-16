0747f4693681f8bae233673d1b0eb8ef7f385377
ling0819 <zxyling@126.com>
2019-05-16 22:58:24 +0800

    .

--- node-study/js/http/server-class.js
+++ node-study/js/http/server-class.js
@@ -1,3 +1,14 @@
+let http = require('http');
+let url = require('url');
+let mime = require('mime');
+
 class Server {
-    
+    handleRequest(req, res) {
+        let pathname = url.parse()
+    }
+
+    start(port) {
+        let server = http.createServer(this.handleRequest.bind(this));
+        server.listen(...arg);
+    }
 }
